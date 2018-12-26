7fa78ab35f80176d3a08bbc36240f1f248729085
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-26 00:03:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ function iLoveTheBeatles(number){
   do{
   array += 'I love the Beatles!'
   }
-  while (array.length = 15 - number);
+  while (
+    if(number > 15){
+      return array
+    }else{
+      (array.length = 15 - number)
+    }
   return array
 }
